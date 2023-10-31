d07ec43360d3b352ea17574e3df7258e7867ffd0
chaimae <chaimae.kanzouai@usmba.ac.ma>
2023-10-31 19:52:54 +0100

    modif

--- src/TP/FirstLast.java
+++ src/TP/FirstLast.java
@@ -1,2 +1,13 @@
-package TP;public class FirstLast {
+package TP;
+
+public class FirstLast {
+    public static void main(String[] args) {
+        int[] array = {6, 3, 7, 9, 10, 1};
+        boolean firstLast = false;
+        if (array.length>=1 && array[0] == array[array.length - 1]){
+            firstLast = true;
+
+        }
+        System.out.println(firstLast);
+    }
 }
--- src/TP/FirstLast6.java
+++ src/TP/FirstLast6.java
@@ -1,2 +1,15 @@
-package TP;public class FirstLast6 {
-}
+package TP;
+
+public class FirstLast6 {
+    public static void main(String[] args) {
+        int[] array = {-1, 3, 7, 9, 10, 1};
+        boolean firstLast = false;
+
+
+            if (((array.length > 0) && (array[0] == 6 || array[array.length - 1] == 6))) {
+                firstLast = true;
+            }
+            System.out.println(firstLast);
+
+        }
+    }
