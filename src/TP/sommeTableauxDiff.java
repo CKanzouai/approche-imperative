442488c476ddb995b79a1726bc372a62028b73ac
chaimae <chaimae.kanzouai@usmba.ac.ma>
2023-10-31 16:03:31 +0100

    Mon

--- src/TP/sommeTableauxDiff.java
+++ src/TP/sommeTableauxDiff.java
@@ -2,14 +2,35 @@ package TP;
 
 public class sommeTableauxDiff {
     public static void main(String[] args) {
-        int[] array1 = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-        int[] array2 ={-1, 12, 17, 14, 5, -9, 0, 18 } ;
-        for (int i = 0; i< array1.length; i++){
-            for (int j =0; j<array2.length; j++){
-                if ((array1[i] || array2[j]) == null){
+        int[] array1 = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+        int[] array2 = {-1, 12, 17, 14, 5, -9, 0, 18};
+        int maxLength = 0;
 
-                }
-            }
+        if (array1.length > array2.length) {
+            maxLength = array1.length;
+        } else {
+            maxLength = array2.length;
         }
+        System.out.println(maxLength);
+
+        for (int i = 0; i < maxLength; i++) {
+            int valeur1 = 0;
+            int valeur2 = 0;
+
+            if (i < array1.length) {
+                valeur1 = array1[i];
+            }
+            if (i < array2.length) {
+                valeur2 = array2[i];
+            }
+
+            int sum = valeur1 + valeur2;
+            System.out.println(sum);
+        }
+
+
+
     }
 }
+
+
